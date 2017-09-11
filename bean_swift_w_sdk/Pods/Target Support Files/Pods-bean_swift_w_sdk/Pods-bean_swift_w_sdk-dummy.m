b70d9665134d701da3a78f5fb388aa11dd436dbe
joseph slater <josephslater@josephs-MacBook-Air.local>
2017-09-11 13:41:00 -0500

    initial bean with sdk

---
+++ bean_swift_w_sdk/Pods/Target Support Files/Pods-bean_swift_w_sdk/Pods-bean_swift_w_sdk-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_bean_swift_w_sdk : NSObject
+@end
+@implementation PodsDummy_Pods_bean_swift_w_sdk
+@end
---
+++ bean_swift_w_sdk/Pods/Target Support Files/Pods-bean_swift_w_sdk/Pods-bean_swift_w_sdk-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_bean_swift_w_sdkVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_bean_swift_w_sdkVersionString[];
+
