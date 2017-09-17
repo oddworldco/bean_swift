ef69a2d9833fd24096c5fb9e5cb823adfad3d5a9
mightygaby <gaby.rf@gmail.com>
2017-09-17 17:06:45 -0500

    successful post request

--- bean_swift_w_sdk/bean_swift_w_sdk/ViewController.swift
+++ bean_swift_w_sdk/bean_swift_w_sdk/ViewController.swift
@@ -2,8 +2,8 @@
 //  ViewController.swift
 //  bean_swift_w_sdk
 //
-//  Created by joseph slater on 9/11/17.
-//  Copyright © 2017 joseph slater. All rights reserved.
+//  Created by Gaby Ruiz-Funes on 9/17/17.
+//  Copyright © 2017 Odd World Co. All rights reserved.
 //
 
 import UIKit
@@ -66,7 +66,7 @@ class ViewController: UIViewController, PTDBeanManagerDelegate, PTDBeanDelegate 
             connectToBean(bean: yourBean!)
             //sendButtonTapped(sender: "test" as AnyObject);
             }
-        postRequest()
+        //postRequest()
     }
     
     // Bean SDK: connects to Bean
@@ -107,14 +107,21 @@ class ViewController: UIViewController, PTDBeanManagerDelegate, PTDBeanDelegate 
             "timeStamp": "currentDate",
             "data": "data",
             ] as [String: Any]
+        // "https://oddworld.herokuapp.com/collect_data"
         
-        var request = URLRequest(url: URL(string: "http://localhost:3000/collect_data")!)
+        var request = URLRequest(url: URL(string: "https://oddworld.herokuapp.com/collect_data")!)
         request.httpMethod = "POST"
         request.httpBody = try! JSONSerialization.data(withJSONObject: customer, options: [])
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
         request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.addValue("application/vnd.marketplace.v1", forHTTPHeaderField: "Accept")
         
-        URLSession.shared.dataTask(with:request, completionHandler: {(data, response, error) in
+        
+        print("begin urlsession")
+        
+        URLSession.shared.dataTask(with:request, completionHandler: {data, response, error in
+            print(response)
+            print("*************")
             if error != nil {
                 print(error)
             } else {
@@ -130,68 +137,8 @@ class ViewController: UIViewController, PTDBeanManagerDelegate, PTDBeanDelegate 
                     }
                 }
             }
+            print("******************************")
         }).resume()
     }
-    //@IBAction func sendButtonTapped(sender: AnyObject)
-    func sendButtonTapped(sender: AnyObject) {
-        print("initialize post function")
-        
-        let userNameValue = "test"
-        
-        // Send HTTP GET Request
-        
-        // Define server side script URL
-        let scriptUrl = "http://localhost:3000/collect_data"
-        
-        // Add one parameter
-        let urlWithParams = scriptUrl + "?userName=\(userNameValue)"
-        
-        // Create NSURL Ibject
-        let myUrl = NSURL(string: urlWithParams);
-        
-        // Creaste URL Request
-        let request = NSMutableURLRequest(url:myUrl! as URL);
-        
-        // Set request HTTP method to GET. It could be POST as well
-        request.httpMethod = "POST"
-        
-        // Excute HTTP Request
-        let task = URLSession.shared.dataTask(with: request as URLRequest) {
-            data, response, error in
-            
-            // Check for error
-            if error != nil
-            {
-                print("error=\(String(describing: error))")
-                return
-            }
-            
-            // Print out response string
-            let responseString = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)
-            print("responseString = \(String(describing: responseString))")
-            
-            
-            // Convert server json response to NSDictionary
-            do {
-                if let convertedJsonIntoDict = try JSONSerialization.jsonObject(with: data!, options: []) as? NSDictionary {
-                    
-                    // Print out dictionary
-                    print(convertedJsonIntoDict)
-                    
-                    // Get value by key
-                    let firstNameValue = convertedJsonIntoDict["userName"] as? String
-                    print(firstNameValue!)
-                    
-                }
-            } catch let error as NSError {
-                print(error.localizedDescription)
-            }
-            
-        }
-        
-        task.resume()
-        
-    }
-
 }
 
