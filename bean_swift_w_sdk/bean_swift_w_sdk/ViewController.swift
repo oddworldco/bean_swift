128cac7f199fac818dc629ee9d2df09ca9749861
mightygaby <gaby.rf@gmail.com>
2017-10-29 17:39:50 -0500

    creates json object to post to DB; deletes timestamp, will move this to back end

--- bean_swift_w_sdk/bean_swift_w_sdk/ViewController.swift
+++ bean_swift_w_sdk/bean_swift_w_sdk/ViewController.swift
@@ -11,18 +11,20 @@ class ViewController: UIViewController,  UITextFieldDelegate, PTDBeanManagerDele
     var beanManager: PTDBeanManager?
     var connectedBean: PTDBean?
     var tempRepeat: Timer!
-    var someData = [NSString]()
+//    var someData = [NSString]()
+    var someData: [String: Any] = [:]
+    var memory = [NSString]()
     var batteryLevel: Int = 0
     
     // Create UI variables
-
+    
     @IBOutlet weak var promptText: UILabel!
     
     //store bean name using core data
     @IBOutlet weak var beanName: UITextField!
     
     @IBOutlet weak var tempOutput: UILabel!
-
+    
     @IBAction func connect(_ sender: Any) {
         if beanName.text == ""  {
             promptText.text = "Please enter name to connect"
@@ -57,13 +59,13 @@ class ViewController: UIViewController,  UITextFieldDelegate, PTDBeanManagerDele
     
     
     // Bean SDK: We check to see if Bluetooth is on.
-//    func beanManagerDidUpdateState(_ beanManager: PTDBeanManager!) {
-//        if beanManager!.state == BeanManagerState.poweredOn {
-//            startScanning()
-//        } else if beanManager!.state == BeanManagerState.poweredOff {
-//            print("Please turn on your bluetooth")
-//        }
-//    }
+    //    func beanManagerDidUpdateState(_ beanManager: PTDBeanManager!) {
+    //        if beanManager!.state == BeanManagerState.poweredOn {
+    //            startScanning()
+    //        } else if beanManager!.state == BeanManagerState.poweredOff {
+    //            print("Please turn on your bluetooth")
+    //        }
+    //    }
     
     // Scan for Peripherals
     func startScanning() {
@@ -101,28 +103,49 @@ class ViewController: UIViewController,  UITextFieldDelegate, PTDBeanManagerDele
     }
     
     // Bean SDK: After connecting to Bean
-
+    
     func beanManager(_ beanManager: PTDBeanManager!, didConnect bean: PTDBean!, error: Error!) {
         self.promptText.text = "Smarty Pants Connected to:"
+        
+        //for i in buffer size
+        //bufferRepeat = Timer.scheduledTimer(timeInterval: 5, target: self, selector: #selector(runUnloadCode), userInfo: nil, repeats: true)
+        
         tempRepeat = Timer.scheduledTimer(timeInterval: 30, target: self, selector: #selector(runTimedCode), userInfo: nil, repeats: true)
-
+        
         print("bean manager called")
-     }
+    }
+    
+    // func runUnloadCode() {
+    //self.connectedBean = bean
+    //    self.connectedBean?.delegate = self
+    //    memory = []
+    //    self.connectedBean?.
+    //    memory.
+    // }
     
     func runTimedCode() {
         //self.connectedBean = bean
         self.connectedBean?.delegate = self
+        someData["name"] = beanName.text!
+//        someData["timeStamp"] = Date()
+//        postRequest(data: someData)
+//        print(someData)
+//        someData = []
         
-        someData.append("'name': '\(beanName.text!)' " as NSString)
-        someData.append("'timeStamp': '\(Date())'}" as NSString)
+//        someData.append("{'name': '\(beanName.text!)' " as NSString)
+//        someData.append("'timeStamp': '\(Date())' " as NSString)
+//        someData.append("'battery': '\(BATTERY_0_PCNT_VOLTAGE)' " as NSString)
         
-        self.connectedBean?.readScratchBank(1)
         self.connectedBean?.readAccelerationAxes()
+        self.connectedBean?.readScratchBank(2)
         self.connectedBean?.readTemperature()
-        self.connectedBean?.readBatteryVoltage()
-
-        postRequest(data: someData)
-        someData = []
+        
+        print(someData);
+        if(someData.count>1){
+            postRequest(data: someData)
+            someData = [:]
+        }
+        //self.connectedBean?.readBatteryVoltage()
     }
     
     func beanManager(_ beanManager: PTDBeanManager!, didDisconnectBean bean: PTDBean!, error: Error!) {
@@ -133,57 +156,69 @@ class ViewController: UIViewController,  UITextFieldDelegate, PTDBeanManagerDele
         tempOutput.text = "Loading..."
     }
     
-    func bean(_ bean: PTDBean!, didUpdateScratchBank bank: Int, data:Data) {
-//        print("********************************")
-//        print("found scratch!")
-//        print(data)
-        let data = Data(bytes: [0x71, 0x3d, 0x0a, 0xd7, 0xa3, 0x10, 0x45, 0x40])
-        let value: Double = data.withUnsafeBytes { $0.pointee }
-//        print(value)
-        someData.append("{'bodyTemp': '\(value)'" as NSString)
-//        print("********************************")
-    }
     
-    func bean(_ bean: PTDBean!, beanDidUpdateBatteryVoltage batteryVoltage: NSNumber) {
-        print("batteryVoltage")
+    
+    //func bean(_ bean: PTDBean!, serialDataReceived serial:NSData!) {
+    //    print(serial)
+    //    let strReceived = NSString(data: serial as Data, encoding: String.Encoding.utf8.rawValue)
+    //    someData.append("'Temp': '\(strReceived)'}" as NSString)
+    //}
+    
+    
+    func bean(_ bean: PTDBean!, didUpdateScratchBank bank: Int, data:Data!) {
+        //       print("********************************")
+        //        print("found scratch!")
+        //print(data)
+        //let bytes = data
+        //let string = String(bytes: data, encoding: .utf8)
+        //var dataString = String(data: bytes!, encoding: .utf16)
+        let dataString: String = String(data: data, encoding: .utf8)! //?? ""
+//        print("raw: '\(dataString)'")
+        if dataString.characters.count > 2 {
+            let first4 = dataString.substring(to:dataString.index(dataString.startIndex, offsetBy: 4))
+//            someData.append("'bodyTemp': '\(first4)' " as NSString)
+              someData["bodyTemp"] = first4
+        } else {
+            let first4 = "0"
+            someData["bodyTemp"] = first4
+//            someData.append("'bodyTemp': '\(first4)' " as NSString)
+        }
+        //print(first4)
+        //let dataString2: Float = dataString
+        //let dataString: String = String(describing: data) //?? ""
+        //someData.append("'bodyTemp': '\(first4)' " as NSString)
+        //        print("********************************")
     }
-
     
     func bean(_ bean: PTDBean!, didUpdateTemperature degrees_celsius: NSNumber!) {
-        print(degrees_celsius)
-        someData.append("'beanTemp': '\(degrees_celsius!)'}" as NSString)
+
+//        someData.append("'beanTemp': '\(degrees_celsius!)' }" as NSString)
+        someData["beanTemp"] = degrees_celsius!
         self.tempOutput.text = degrees_celsius.stringValue
     }
     
     
     func bean(_ bean: PTDBean!, didUpdateAccelerationAxes acceleration: PTDAcceleration) {
-        someData.append("'X': '\(acceleration.x)', 'Y': '\(acceleration.y)', 'Z': '\(acceleration.z)'"as NSString)
-        // Show acceleration
-//        print( "********************************")
-//        print( "X: \(acceleration.x); Y: \(acceleration.y); Z: \(acceleration.z)" )
-//        print( "********************************")
+//        someData.append("'X': '\(acceleration.x)', 'Y': '\(acceleration.y)', 'Z': '\(acceleration.z)' "as NSString)
+        someData["X"] = acceleration.x
+        someData["Y"] = acceleration.y
+        someData["Z"] = acceleration.z
     }
     
-    //post request:
-//    func createObj(temp: NSNumber!) {
-//        let temp = temp
-//        print("DATA!")
-//        //postRequest(temp: temp)
-//    }
     
-    func postRequest(data: [NSString]) {
+    func postRequest(data: Any) {
         print("post request!!!!!!!!!")
         
         print("SOMEDATA")
         print(data)
         
-//        let data = [
-//            "uuid": "uuid",
-//            "timeStamp": "currentDate",
-//            "temp": temp,
-//            ] as [String: Any]
+        //        let data = [
+        //            "uuid": "uuid",
+        //            "timeStamp": "currentDate",
+        //            "temp": temp,
+        //            ] as [String: Any]
         // "https://oddworld.herokuapp.com/collect_data"
-        var request = URLRequest(url: URL(string: "https://oddworld.herokuapp.com/ios_data")!)
+        var request = URLRequest(url: URL(string: "https://oddworld.herokuapp.com/ios_test")!)
         request.httpMethod = "POST"
         request.httpBody = try! JSONSerialization.data(withJSONObject: data, options: [])
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
@@ -196,21 +231,21 @@ class ViewController: UIViewController,  UITextFieldDelegate, PTDBeanManagerDele
         URLSession.shared.dataTask(with:request, completionHandler: {data, response, error in
             print(response)
             print("*************")
-//            if error != nil {
-//                print(error)
-//            } else {
-//                do {
-//                    guard let json = try? JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? [String: Any] else { return }
-//
-//                    guard let errors = json?["errors"] as? [[String: Any]] else { return }
-//                    if errors.count > 0 {
-//                        // show error
-//                        return
-//                    } else {
-//                        // show confirmation
-//                    }
-//                }
-//            }
+            //            if error != nil {
+            //                print(error)
+            //            } else {
+            //                do {
+            //                    guard let json = try? JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? [String: Any] else { return }
+            //
+            //                    guard let errors = json?["errors"] as? [[String: Any]] else { return }
+            //                    if errors.count > 0 {
+            //                        // show error
+            //                        return
+            //                    } else {
+            //                        // show confirmation
+            //                    }
+            //                }
+            //            }
             print("******************************")
         }).resume()
     }
@@ -221,7 +256,7 @@ class ViewController: UIViewController,  UITextFieldDelegate, PTDBeanManagerDele
     //create service function that gets user data to define object we're posting to
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+        // Dispose of any resources that can be recreated.
     }
 }
 
